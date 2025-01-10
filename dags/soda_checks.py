324c323c7e4b115f941a27fab69a207bb1727e84
stunner007-ir <ishu9693raj@gmail.com>
2025-01-10 12:22:56 +0530

    worked on soda part

--- dags/soda_checks.py
+++ dags/soda_checks.py
@@ -5,20 +5,23 @@ from datetime import datetime
 
 # Define default arguments
 default_args = {
-    'start_date': datetime(2023, 1, 1),
-    'catchup': False,
+    "start_date": datetime(2023, 1, 1),
+    "catchup": False,
 }
 
-@task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+
+@task.external_python(python="/usr/local/airflow/soda_venv/bin/python")
 def run_soda_checks(scan_name, checks_subpath):
     from include.soda.checks.check_function import check
+
     return check(scan_name, checks_subpath)
 
+
 @dag(
-    dag_id='soda_checks',
+    dag_id="soda_checks",
     default_args=default_args,
     schedule_interval=None,  # No schedule, manual trigger
-    tags=['example'],
+    tags=["example"],
 )
 def soda_checks_pipeline():
     """
@@ -26,15 +29,13 @@ def soda_checks_pipeline():
     """
 
     check_load = PythonOperator(
-        task_id='check_load',
+        task_id="check_load",
         python_callable=run_soda_checks,
-        op_kwargs={
-            'scan_name': 'check_load',
-            'checks_subpath': 'include/soda/checks'
-        },
+        op_kwargs={"scan_name": "check_load", "checks_subpath": "include/soda/checks"},
     )
 
     check_load
+
 
 # Instantiate the DAG
 soda_checks_dag = soda_checks_pipeline()
